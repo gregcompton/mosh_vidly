79379daafc93cde9fe609cc5a8bfff24e930766b
gregcompton <greg@gregcompton.com>
2019-09-20 14:13:25 -0400

    reimplemented validateProperty in loginForm w/ Joi

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -28,14 +28,10 @@ class LoginForm extends Component {
   };
 
   validateProperty = ({ name, value }) => {
-    if (name === "username") {
-      if (value.trim() === "") return "Username is required.";
-      //...other rules here
-    }
-    if (name === "password") {
-      if (value.trim() === "") return "Password is required.";
-      //...other rules here
-    }
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
   };
 
   handleSubmit = e => {
