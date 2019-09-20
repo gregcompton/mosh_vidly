0ebb8e6d9b5d84e2f547539e71c79e16583f3e47
gregcompton <greg@gregcompton.com>
2019-09-20 13:37:30 -0400

    Basic validation on change

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -21,6 +21,17 @@ class LoginForm extends Component {
     return Object.keys(errors).length === 0 ? null : errors;
   };
 
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") return "Username is required.";
+      //...other rules here
+    }
+    if (name === "password") {
+      if (value.trim() === "") return "Password is required.";
+      //...other rules here
+    }
+  };
+
   handleSubmit = e => {
     e.preventDefault();
 
@@ -33,9 +44,14 @@ class LoginForm extends Component {
   };
 
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
 
   render() {
