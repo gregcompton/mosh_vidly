41147e3c0eea395c672039ea03b7a20c3ebb9d44
gregcompton <greg@gregcompton.com>
2019-09-20 13:25:12 -0400

    Basic error handling implemented for loginForm

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,13 +1,33 @@
 import React, { Component } from "react";
 import Input from "./common/input";
+import { EROFS } from "constants";
 
 class LoginForm extends Component {
   state = {
-    account: { username: "", password: "" }
+    account: { username: "", password: "" },
+    errors: {}
+  };
+
+  validate = () => {
+    const errors = {};
+
+    const { account } = this.state;
+
+    if (account.username.trim() === "")
+      errors.username = "Username is required.";
+    if (account.password.trim() === "")
+      errors.password = "Password is required.";
+
+    return Object.keys(errors).length === 0 ? null : errors;
   };
 
   handleSubmit = e => {
     e.preventDefault();
+
+    const errors = this.validate();
+    console.log(errors);
+    this.setState({ errors });
+    if (errors) return;
 
     //Call the server etc.
     console.log("Submitted");
@@ -20,7 +40,7 @@ class LoginForm extends Component {
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
 
     return (
       <div>
@@ -31,12 +51,14 @@ class LoginForm extends Component {
             value={account.username}
             label="Username"
             onChange={this.handleChange}
+            error={errors.username}
           />
           <Input
             name="password"
             value={account.password}
             label="Password"
             onChange={this.handleChange}
+            error={errors.password}
           />
 
           <button className="btn btn-primary">Login</button>
